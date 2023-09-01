026f1ceb761690ebe4fc55375774f031317b04c8
Christoph Stelz <christoph.stelz@student.kit.edu>
2023-09-01 11:00:16 +0200

    Explicitly state library version

--- test-build.sh
+++ test-build.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-${CMAKE:-cmake} -B build -DUSE_MPI=ON -DUSE_REPRODUCIBLE=ON -DCMAKE_BUILD_TYPE=Release -S .
+${CMAKE:-cmake} -B build -DUSE_MPI=ON \
+    -DUSE_REPRODUCIBLE=ON -D binarytreesummation_version=929f8456dd8ab1ea15b16d102038c8d1505c2e26 \
+    -DCMAKE_BUILD_TYPE=Release -S .
 make -C build -j$(nproc)
