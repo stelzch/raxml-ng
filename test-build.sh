9d261d2c6e3aed6d19f404fdf419986aa0b3f620
Christoph Stelz <christoph.stelz@student.kit.edu>
2023-11-17 09:36:26 +0100

    Update version of binarytreesummatio library

--- test-build.sh
+++ test-build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 ${CMAKE:-cmake} -B build -DUSE_MPI=ON \
-    -DUSE_REPRODUCIBLE=ON -D binarytreesummation_version=4d3f3a98918458bf6e7fb5c0be4bc36cccb2e51e \
+    -DUSE_REPRODUCIBLE=ON -D binarytreesummation_version=47ab47c54b3f32144dbe2a0620d9a3d77588c167 \
     -DCMAKE_BUILD_TYPE=Release -S .
 make -C build -j$(nproc)
