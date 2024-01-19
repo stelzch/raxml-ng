46e417b18a667601a189fccda676778c8aaa8336
Christoph Stelz <christoph.stelz@student.kit.edu>
2024-01-19 09:33:25 +0100

    Add run script

---
+++ R.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+nproc=$1
+if [ -n "$nproc" ] && [ "$nproc" -eq "$nproc" ] 2> /dev/null; then
+    echo Using $nproc processors 
+else
+    echo Must specify number of cores as first argument.
+    exit 1
+fi
+mpirun \
+    --use-hwthread-cpus \
+	-np $nproc build/bin/raxml-ng-mpi --prefix R_p$nproc \
+	--redo \
+    --log DEBUG --precision 50\
+	--seed 42 \
+	--msa ../test-Datasets/DNA-Data/354/354.phy \
+	--pat-comp off \
+    --site-repeats off \
+    --tip-inner off \
+	--workers 1 \
+	--model GTR+G \
+	--tree rand{1}
