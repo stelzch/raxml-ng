d8f4dacd4930a227e48a8bab9e5047a7684b3717
Christoph Stelz <christoph.stelz@student.kit.edu>
2024-05-27 09:13:40 +0200

    Update pll-modules and ipc-debug

--- test-build.sh
+++ test-build.sh
@@ -2,7 +2,7 @@
 ${CMAKE:-cmake} -B build -DUSE_MPI=ON -DBUILD_TESTS=OFF \
     -DUSE_REPRODUCIBLE=ON \
     -Dbinarytreesummation_version=fbb5e8d7d24eedfcfb4b5650b4213da9ba86e1b5 \
-    -Dipcdebug_version=11f7aaf756dde008bd9bf80673cd518f9904c54f \
+    -Dipcdebug_version=8cf2664687d175037f3b42fd4359dde2ec4a50e3 \
     -DFETCHCONTENT_QUIET=OFF \
     -DCMAKE_BUILD_TYPE=Release -DCMAKE_EXPORT_COMPILE_COMMANDS=YES -S .
 make -C build -j$(nproc)
