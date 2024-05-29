89d6f7ce8a14aca6c2a56bb18b755b23184e525c
Christoph Stelz <christoph.stelz@student.kit.edu>
2024-05-29 08:29:47 +0200

    Unify run script

--- Rm.sh
+++ Rm.sh
@@ -1,14 +1,25 @@
 #!/bin/bash
 nproc=$1
+simd=$2
+variant="build-$3"
 if [ -n "$nproc" ] && [ "$nproc" -eq "$nproc" ] 2> /dev/null; then
     echo Using $nproc processors 
 else
     echo Must specify number of cores as first argument.
     exit 1
 fi
+if [[ -z "$2" ]]; then
+    echo Must specify SIMD set as first argument: none, sse, avx, avx2
+    exit 1
+fi
+if [[ -z "$3" ]]; then
+    variant="build-nonrepr"
+fi
+out=R_partial_avx2_${PREFIX}_p${nproc} 
+echo Prefix is $out, build folder $variant
 mpirun \
     --use-hwthread-cpus \
-	-np $nproc build/bin/raxml-ng-mpi --prefix R_p$nproc \
+	-np $nproc $variant/bin/raxml-ng-mpi --prefix $out \
 	--redo \
     --log DEBUG --precision 50\
 	--seed 42 \
@@ -16,7 +27,7 @@ mpirun \
 	--pat-comp off \
     --site-repeats off \
     --tip-inner off \
-    --simd none \
+    --simd $simd \
 	--workers 1 \
     --threads 1 \
 	--model GTR+G \
