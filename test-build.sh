0dca1674f0f573a8b9e3fd14aa856f77feb48307
Christoph Stelz <christoph.stelz@student.kit.edu>
2024-06-19 10:55:11 +0200

    Disable ubsan, reduce verboseness of build

--- test-build.sh
+++ test-build.sh
@@ -5,18 +5,15 @@ ${CMAKE:-cmake} -B build-bts -DUSE_MPI=ON -DBUILD_TESTS=OFF \
     -DREPRODUCIBLE=BINARYTREESUMMATION \
     -Dbinarytreesummation_version=$binarytreesummation_version \
     -Dipcdebug_version=$ipcdebug_version \
-    -DFETCHCONTENT_QUIET=OFF \
     -DCMAKE_BUILD_TYPE=Release -DCMAKE_EXPORT_COMPILE_COMMANDS=YES -S .
 ${CMAKE:-cmake} -B build-reproblas -DUSE_MPI=ON -DBUILD_TESTS=OFF \
     -DREPRODUCIBLE=REPROBLAS \
     -Dbinarytreesummation_version=$binarytreesummation_version \
     -Dipcdebug_version=$ipcdebug_version \
-    -DFETCHCONTENT_QUIET=OFF \
     -DCMAKE_BUILD_TYPE=Release -DCMAKE_EXPORT_COMPILE_COMMANDS=YES -S .
 ${CMAKE:-cmake} -B build-nonrepr -DUSE_MPI=ON -DBUILD_TESTS=OFF \
     -Dbinarytreesummation_version=$binarytreesummation_version \
     -Dipcdebug_version=$ipcdebug_version \
-    -DFETCHCONTENT_QUIET=OFF \
     -DCMAKE_BUILD_TYPE=Release -DCMAKE_EXPORT_COMPILE_COMMANDS=YES -S .
 for folder in build-bts build-reproblas build-nonrepr
 do
